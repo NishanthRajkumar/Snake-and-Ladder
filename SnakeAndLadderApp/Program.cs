a06ddfa02080e2caa3f0b82a66254ad09e2a45c9
Nishanth <rajkumarnishanth@gmail.com>
2022-02-09 11:41:48 +0530

    Added RollDie method

--- SnakeAndLadderApp/Program.cs
+++ SnakeAndLadderApp/Program.cs
@@ -4,4 +4,6 @@ Console.Title = "Snake & Ladder";
 Console.WriteLine("------------------Snake and Ladder Simulator------------------");
 SnakeAndLadder player1 = new SnakeAndLadder();
 player1.Describe();
+player1.RollDie();
+player1.Describe();
 Console.ReadKey();
