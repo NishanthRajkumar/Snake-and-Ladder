ee42da6a845e29884289c705c078054ce8af4314
Nishanth <rajkumarnishanth@gmail.com>
2022-02-09 12:43:12 +0530

    Added method to play till the player has won

--- SnakeAndLadderApp/Program.cs
+++ SnakeAndLadderApp/Program.cs
@@ -4,5 +4,5 @@ Console.Title = "Snake & Ladder";
 Console.WriteLine("------------------Snake and Ladder Simulator------------------");
 SnakeAndLadder player1 = new SnakeAndLadder();
 player1.Describe();
-player1.RollDie();
+player1.PlayTillEnd();
 Console.ReadKey();
--- SnakeAndLadderApp/SnakeAndLadder.cs
+++ SnakeAndLadderApp/SnakeAndLadder.cs
@@ -30,14 +30,15 @@ internal class SnakeAndLadder
     public void RollDie()
     {
         int roll = random.Next(1,7);
-        Console.WriteLine("Rolled: " + roll);
+        Console.WriteLine("\nRolled: " + roll);
         position += roll;
         Option(roll);
+        CheckBoundary();
         Describe();
     }
 
     // Checks option for player using Random
-    public void Option(int roll)
+    private void Option(int roll)
     {
         int option = random.Next(0,3);
         switch (option)
@@ -67,5 +68,22 @@ internal class SnakeAndLadder
         Console.WriteLine("Yay! A Ladder!!!");
         Console.WriteLine("Going up by " + roll);
         position += roll;
+    }
+
+    // Checks if position is out of bounds in the game
+    private void CheckBoundary()
+    {
+        if (position < 0)
+            position = 0;
+        else if (position >= 100)
+            position = 100;
+    }
+
+    // This will roll die untill player wins
+    public void PlayTillEnd()
+    {
+        while (position < 100)
+            RollDie();
+        Console.WriteLine("\nCongratulations!! You Won!!");
     }
 }
