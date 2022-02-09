4b4051ffa08bc63999497dc3ef0e7e3c0caf2e93
Nishanth <rajkumarnishanth@gmail.com>
2022-02-09 14:41:45 +0530

    Added player 2 to play till one player wins

--- SnakeAndLadderApp/Program.cs
+++ SnakeAndLadderApp/Program.cs
@@ -3,6 +3,10 @@
 Console.Title = "Snake & Ladder";
 Console.WriteLine("------------------Snake and Ladder Simulator------------------");
 SnakeAndLadder player1 = new SnakeAndLadder();
+SnakeAndLadder player2 = new SnakeAndLadder();
+Console.WriteLine("Player 1: ");
 player1.Describe();
-player1.PlayTillEnd();
+Console.WriteLine("Player 2: ");
+player2.Describe();
+player1.PlayTillEnd(player2);
 Console.ReadKey();
