f55777c1ae4b495c9c2d9adf922643230c1d528c
Nishanth <rajkumarnishanth@gmail.com>
2022-02-09 11:35:42 +0530

    Initialized the game for each player with poistion as 0

--- SnakeAndLadderApp/Program.cs
+++ SnakeAndLadderApp/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Title = "Snake & Ladder";
-Console.WriteLine("------------------Snake and Ladder Simulator------------------");
-Console.ReadKey();
+﻿using SnakeAndLadderApp;
 
+Console.Title = "Snake & Ladder";
+Console.WriteLine("------------------Snake and Ladder Simulator------------------");
+SnakeAndLadder player1 = new SnakeAndLadder();
+player1.Describe();
+Console.ReadKey();
---
+++ SnakeAndLadderApp/SnakeAndLadder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderApp;
+
+internal class SnakeAndLadder
+{
+    // Game related attributes declared here
+    int position;
+
+    // Default Constructor
+    public SnakeAndLadder()
+    {
+        position = 0;
+    }
+
+    public void Describe()
+    {
+        Console.WriteLine("Position: " + position);
+    }
+}
